8e132fb948a44ceb81a2ead3c3d8033fd9bf5833
veselyroman <veselyroman@seznam.cz>
2026-09-09 14:51:06 +0200

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,3 +17,19 @@ firebase.initializeApp({
 
 const messaging =
     firebase.messaging();
+
+messaging.onBackgroundMessage(
+    (payload) => {
+
+        self.registration.showNotification(
+            payload.notification.title,
+            {
+                body:
+                    payload.notification.body,
+                icon:
+                    "/image/icon-192.png"
+            }
+        );
+
+    }
+);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bandplanner-v25";
+const CACHE_NAME = "bandplanner-v26";
 
 const urlsToCache = [
     "./",
